353e99ee4960732715f9acaa2614c7ae58aa913e
OxanaSF <oxanatango@gmail.com>
2022-12-20 14:43:26 -0800

    add Country class to models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,6 +6,11 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -18,12 +23,11 @@ class Address(models.Model):
         verbose_name_plural = "Address Entries"
 
 
-
-
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -32,18 +36,18 @@ class Author(models.Model):
         return self.full_name()
 
 
-
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country, )
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
